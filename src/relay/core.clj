4c8c8bf8b298554d4850f11c3abf193f420c0d91
Thanh <thanhvg@gmail.com>
2024-04-23 06:58:36 -0600

    work

--- src/relay/core.clj
+++ src/relay/core.clj
@@ -11,10 +11,33 @@
             [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :refer [response]]
             [clojure.data.xml :as xml]
-            [clojure.data.codec.base64 :as base64]
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
+
+(defn deflate-string [s]
+  (let [bytes (.getBytes s "UTF-8")
+        deflater (Deflater. 9 true)  ; 9 is the highest compression level
+        bos (ByteArrayOutputStream. 1024)
+        dos (DeflaterOutputStream. bos deflater)]
+    (.write dos bytes)
+    (.close dos)
+    (.encodeToString (java.util.Base64/getEncoder) (.toByteArray bos))))
+
+
+(defn change-xml [xml-str]
+  (let [data (xml/parse-str xml-str)]
+    (xml/emit-str
+     (assoc-in data [:attrs :Destination] "https://sso.awsdev.pason.com/saml2/logout"))))
+
+
+(defn inflate-base64 [base64-str]
+  (let [decoder (java.util.Base64/getDecoder)
+        byte-arr (.decode decoder base64-str)
+        byte-arr-input-stream (io/input-stream byte-arr)
+        inflater (Inflater. true) ; Specify true if the compressed data includes a header
+        inflater-stream (InflaterInputStream. byte-arr-input-stream inflater)]
+    (slurp inflater-stream)))
 
 (defn html-response [params]
   ;; (print params)
@@ -51,6 +74,42 @@
   (-> html-response
       wrap-params))
 
+(defn html-response-to-cognito [params]
+  ;; (print params)
+  (let [sam-res (get-in params [:query-params "SAMLResponse"])
+        my-sam-res (deflate-string (change-xml (inflate-base64 sam-res)))
+        relay-state (get-in params [:query-params "RelayState"])
+        signature (get-in params [:query-params "Signature"])
+        sig-alg (get-in params [:query-params "SigAlg"])]
+    (response
+     (str "<html>"
+          "<head>"
+          "<meta charset=\"utf-8\" />"
+          "</head>"
+          "<body>"
+          "<form action=\"https://sso.awsdev.pason.com/saml2/logout\" method=\"post\">"
+          "<div>"
+          "<input name=\"SAMLResponse\" value=\"" my-sam-res "\"/>"
+          "<input name=\"RelayState\" value=\"" relay-state "\"/>"
+          "<input name=\"Signature\" value=\"" signature "\"/>"
+          "<input name=\"SigAlg\" value=\"" sig-alg "\"/>"
+          "</div>"
+          "<div>"
+          "<input type=\"submit\" value=\"Continue\"/>"
+          "</div>"
+          "<noscript>"
+          "<div>"
+          "<input type=\"submit\" value=\"Continue\"/>"
+          "</div>"
+          "</noscript>"
+          "</form>"
+          "</body>"
+          "</html>"))))
+
+(def app-to-cognito
+  (-> html-response-to-cognito
+      wrap-params))
+
 
 ;; (defn handler [request]
 ;;   (print request)
@@ -58,7 +117,7 @@
 
 
 (defn -main []
-  (jetty/run-jetty app {:port 3000}))
+  (jetty/run-jetty app-to-cognito {:port 3000}))
 
 ;; (-main)
 
@@ -84,11 +143,6 @@
 ;;         (zip-xml/xml-> :content)
 ;;         (xml/emit-str)
 ;;         )))
-
-(defn change-xml [xml-str]
-  (let [data (xml/parse-str xml-str)]
-    (xml/emit-str
-     (assoc-in data [:attrs :Destination] "https://sso.awsdev.pason.com/saml2/logout"))))
 
 
 (def xml-input "<samlp:LogoutResponse ID=\"_3b9ef597-f496-461c-bc63-ef5448c317d7\" Version=\"2.0\" IssueInstant=\"2024-04-23T01:45:13.665Z\" Destination=\"https://sso.awsdev.pason.com/saml2/logout\" InResponseTo=\"_bcaea5e3-97c1-4d7b-a5da-10ed09c7dde0\" xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\"><Issuer xmlns=\"urn:oasis:names:tc:SAML:2.0:assertion\">https://sts.windows.net/c655950e-7130-45b5-b9c1-88f9a658b29a/</Issuer><samlp:Status><samlp:StatusCode Value=\"urn:oasis:names:tc:SAML:2.0:status:Success\"/></samlp:Status></samlp:LogoutResponse>")
@@ -129,32 +183,3 @@
 ;;     (String. (.toByteArray ((.getBuffer output-stream))) "UTF-8")))
 
 
-;; (defn deflate-string [s]
-;;   (let [bytes (.getBytes s "UTF-8")
-;;         deflater (Deflater. 9 true)  ; 9 is the highest compression level
-;;         bos (ByteArrayOutputStream. 1024)
-;;         dos (DeflaterOutputStream. bos deflater)]
-;;     (.write dos bytes)
-;;     (.close dos)
-;;     (.encodeToString (java.util.Base64/getEncoder) (.toByteArray bos))))
-
-(defn inflate-string [s]
-  (let [bytes (.decode (java.util.Base64/getDecoder) s;; (.getBytes s "UTF-8")
-                       )
-        bis (ByteArrayInputStream. bytes)
-        iis (InflaterInputStream. bis)
-        bos (ByteArrayOutputStream. 1024)]
-    (loop [b (.read iis)]
-      (when (not= b -1)
-        (.write bos b)
-        (recur (.read iis))))
-    (.toString (.toByteArray bos) "UTF-8")))
-
-
-(defn inflate-base64 [base64-str]
-  (let [decoder (java.util.Base64/getDecoder)
-        byte-arr (.decode decoder base64-str)
-        byte-arr-input-stream (io/input-stream byte-arr)
-        inflater (Inflater. true) ; Specify true if the compressed data includes a header
-        inflater-stream (InflaterInputStream. byte-arr-input-stream inflater)]
-    (slurp inflater-stream)))
